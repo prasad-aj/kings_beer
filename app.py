a31e2ba9929c32bd980fd3bb3e04c654b1de97a6
Prasad <prasad.aj73@gmail.com>
2022-09-19 01:16:10 +0530

    Add existing file

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_ckeditor import CKEditor
 app = Flask(__name__)
 
 ckeditor = CKEditor(app)
-app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///drinkup_database.db'
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kb_database.db'
 app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know"
 
 UPLOAD_FOLDER = 'static/images/'
